008bc7e91931b6a7ef709bdd34d2394e05f955e5
hunter-hues <98989668+hunter-hues@users.noreply.github.com>
2025-03-24 16:47:31 -0700

    Updated app.py
    added port default

--- RaffleBot/app.py
+++ RaffleBot/app.py
@@ -420,4 +420,5 @@ def winnings():
     return render_template("winnings.html", winnings=winnings)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
